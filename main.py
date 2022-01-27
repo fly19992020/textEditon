1669e9caa4e36b206319f4821f6903c891109393
Yyuhang <93079590+fly19992020@users.noreply.github.com>
2022-01-27 19:02:18 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,14 +1,27 @@
-a = """"""
 th = 0
 file = open(input("Enter the address of the file to be imported>>"), "w")
-print("________________________________________")
+print("__________")
+strs = {}    #head
+
+
+def h(a):
+    s = input(str(a))
+    strs[a] = s
+    return 0
 
 
 while True:
     th += 1
     n = input(str(th) + " ")
-    if n == "q":
-        file.write(a)
+    if n == "QUIT":
         break
+    elif n[0] == "i":
+        h(n[1:])
+        th -= 1
     else:
-        a += "\n" + n
+        strs[str(th)] = n  #main
+
+
+for a in strs:
+    file.write(strs[a])
+    file.write("\r")    #write
